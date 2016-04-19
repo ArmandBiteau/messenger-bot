6c7e77364d5d30fd9f1692beb0a92920447625e7
Armand Biteau <armand.biteau@mediamonks.com>
2016-04-19 14:23:05 +0100

    updated the bot to speak

--- server/index.js
+++ server/index.js
@@ -124,6 +124,8 @@ app.get('/webhook/', function (req, res) {
 
 app.post('/webhook/', function (req, res) {
 
+	console.log(req);
+
     var messaging_events = req.body.entry[0].messaging;
 
     for (i = 0; i < messaging_events.length; i++) {
