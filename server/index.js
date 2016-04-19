e05b77e69abf7dbd1f6150fa4e21b81d4c85e6d6
Armand Biteau <armand.biteau@mediamonks.com>
2016-04-19 14:03:26 +0100

    updated the bot to speak

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json({
 
 
 function sendTextMessage(sender, text) {
-    messageData = {
+    var messageData = {
         text:text
     };
     request({
@@ -60,30 +60,30 @@ app.get('/webhook/', function (req, res) {
 
         res.send(req.query['hub.challenge']);
 
-    } else {
-
-		messaging_events = req.body.entry[0].messaging;
-
-		for (i = 0; i < messaging_events.length; i++) {
-
-			let event = req.body.entry[0].messaging[i];
-
-			sender = event.sender.id;
-
-	        if (event.message && event.message.text) {
-
-	            text = event.message.text;
-	            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200));
-
-	        }
-
-	    }
-
-	    res.sendStatus(200);
-
-	}
+    }
 
     res.send('Error, wrong token');
+
+});
+
+app.post('/webhook/', function (req, res) {
+	
+    var messaging_events = req.body.entry[0].messaging;
+
+    for (i = 0; i < messaging_events.length; i++) {
+
+        let event = req.body.entry[0].messaging[i];
+        sender = event.sender.id;
+
+        if (event.message && event.message.text) {
+
+            text = event.message.text;
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200));
+
+        }
+    }
+
+    res.sendStatus(200);
 
 });
 
