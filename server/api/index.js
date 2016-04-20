64be8c85da6705f5920544d00269b0b4b277bc03
Armand Biteau <armand.biteau@mediamonks.com>
2016-04-20 11:00:36 +0100

    update heroku

--- server/api/index.js
+++ server/api/index.js
@@ -12,30 +12,36 @@ class Wit {
         return new Promise((resolve, reject) => {
 
             var data = {
-                sender: '',
-                content: '',
+                sender: req.body.entry[0].messaging[0].sender.id,
+                content: 'test',
                 postback: false
             };
 
-            var event = req.body.entry[0].messaging[0];
-
-            // get sender id
-            data.sender = event.sender.id;
-
-            if (event.message && event.message.text) {
-
-                data.content = event.message.text.toLowerCase();
-
-            }
-
-    		if (event.postback) {
-
-                data.content = event.postback.payload.toLowerCase();
-                data.postback = true;
-
-            }
-
-            console.log('Analyse done !');
+            // for (var i = 0; i < req.body.entry[0].messaging.length; i++) {
+            //
+            //     var event = req.body.entry[0].messaging[i];
+            //
+            //     // get sender id
+            //     data.sender = event.sender.id;
+            //
+            //     if (event.message && event.message.text) {
+            //
+            //         data.content = event.message.text.toLowerCase();
+            //
+            //     }
+            //
+        	// 	if (event.postback) {
+            //
+            //         data.content = event.postback.payload.toLowerCase();
+            //         data.postback = true;
+            //
+            //     }
+            //
+            //     console.log('Analyse done !');
+            //
+            //     resolve(data);
+            //
+            // }
 
             resolve(data);
 
