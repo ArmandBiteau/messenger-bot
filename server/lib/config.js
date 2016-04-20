482d20ab3e3e506e3c97107a5aae5b1ab3e4f3d7
Armand Biteau <armand.biteau@mediamonks.com>
2016-04-20 09:49:18 +0100

    update heroku

---
+++ server/lib/config.js
@@ -0,0 +1,3 @@
+export const PAGE_TOKEN = 'EAAH5YDy9ozYBALgpeRXcGExCGZCYHnLG9gBdOlpBNCxqpl2jn8pkiCZAkm1qsB1jZA6AHkfywx7u4LLqddigfbCcf5hBHrf6lq6VEIiXL8mV1ka1nPxRsbAPHfXLBZCZCFpPSIdDc4xHFKZAf3FaIrhlqNRr1MPyOg5Jma309fRwZDZD';
+
+export const WIT_TOKEN = '57173e9d-d806-4208-915d-cfd8e3ecc849';
